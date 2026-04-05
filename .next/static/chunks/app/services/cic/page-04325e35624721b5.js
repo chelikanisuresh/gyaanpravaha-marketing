3544d588998aedb92d0bb3c13a1b29973e9bcfaf
Suresh Chelikani <cvvsureshbabu@gmail.com>
2026-04-05 16:36:22 +0530

    Fix: add .gitignore, remove node_modules from tracking

--- .next/static/chunks/app/services/cic/page-04325e35624721b5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[28,160,405,578,931,301,832,751,649,689,341,449,659,935,153],{1585:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=1585)}),_N_E=n.O()}]);
